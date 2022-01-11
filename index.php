a0030ab2f4ad6cd1e2f66146c898d08973f76445
r3card0 <ricardo.rdz@pm.me>
2022-01-11 00:05:11 -0600

    started to learn php code

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php # etiqueta que declara que se inicia php
+
+//echo "Hola Mundo \n";
+
+# declarar variables
+$nombre = "Amy";
+$apellido = "Lee";
+
+//echo "Si, ella se llama " . $nombre . " " . $apellido . "\n";
+
+// concatenación mas sencilla :)
+// con comillas dobles PHP es capaz de identificar las variables :)
+echo "Si, ella se llama $nombre $apellido \n";
+
+// operaciones matemáticas
+echo "El resultado de 45 * 3 es = ". 45*5 . "\n";
+
+// mejor opcion en operaciones aritmeticas uso de parentesis ( )
+
+echo "El resultado de 56 + 67 = " . (56+67) . "\n";
